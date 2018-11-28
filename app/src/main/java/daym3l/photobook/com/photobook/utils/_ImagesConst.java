c27c8bb70823736df14b4679dcaddfc89426e111
Daymel <daym3l@nauta.cu>
2018-11-27 22:10:21 -0500

    asd

--- app/src/main/java/daym3l/photobook/com/photobook/utils/_ImagesConst.java
+++ app/src/main/java/daym3l/photobook/com/photobook/utils/_ImagesConst.java
@@ -10,12 +10,13 @@ public class _ImagesConst {
     public final static int[] IMAGES_SLIDER = new int[]{
             R.drawable.image1, R.drawable.image2, R.drawable.image3,
             R.drawable.image4, R.drawable.image5, R.drawable.image6,
-            R.drawable.image7,  R.drawable.image9,R.drawable.image25,
-            R.drawable.image10, R.drawable.image11,  R.drawable.image13,
+            R.drawable.image7, R.drawable.image8, R.drawable.image9,
+            R.drawable.image10, R.drawable.image11, R.drawable.image12, R.drawable.image13,
             R.drawable.image14, R.drawable.image15, R.drawable.image16,
-            R.drawable.image17, R.drawable.image18,R.drawable.image24,
-            R.drawable.image20, R.drawable.image21,  R.drawable.image23,
-            R.drawable.image26,
+            R.drawable.image17, R.drawable.image18, R.drawable.image19, R.drawable.image20,
+            R.drawable.image21, R.drawable.image22, R.drawable.image23,
+            R.drawable.image24, R.drawable.image25, R.drawable.image26
+
 
 
     };
